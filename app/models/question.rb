c49be00f3610333104b59a83e8f677d44fca1f01
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-12-10 14:07:23 -0800

    Fix order of dependent option declaration in Question model. Ensure that the dependent option is specified after the scope for question options to maintain clarity and consistency in associations.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   # Associations
   belongs_to :profile_domain
-  has_many :question_options, dependent: :destroy, -> { order(:position) }
+  has_many :question_options, -> { order(:position) }, dependent: :destroy
   has_many :answers, dependent: :destroy
 
   # Enums
